b29f2b90a14878e4ef3f13d548ac2b5145194e2c
alfahadarc <alfahadarc@gmail.com>
2022-08-15 15:44:18 +0600

    email done

--- esp_site_ccs811_mq/esp_site_ccs811_mq.ino
+++ esp_site_ccs811_mq/esp_site_ccs811_mq.ino
@@ -4,6 +4,30 @@
 #include <SPIFFS.h>
 #include <Wire.h>
 
+//for mail
+#include <Arduino.h>
+
+#include <ESP_Mail_Client.h>
+
+#define WIFI_SSID "Aronnok"
+#define WIFI_PASSWORD "337077dark_world"
+
+#define SMTP_HOST "smtp.gmail.com"
+#define SMTP_PORT 465
+
+/* The sign in credentials */
+#define AUTHOR_EMAIL "espcontroller32@gmail.com"
+#define AUTHOR_PASSWORD "axrbrmdatvuzextw"
+
+/* Recipient's email*/
+#define RECIPIENT_EMAIL "alfahadarc@gmail.com"
+
+/* The SMTP Session object used for Email sending */
+SMTPSession smtp;
+
+/* Callback function to get the Email sending status */
+void smtpCallback(SMTP_Status status);
+
 // comm from arduino
 #define RXp2 16
 #define TXp2 17
@@ -11,6 +35,8 @@
 
 // globals
 String CO2, Toluene, NH4, Acetone, TVOC, WL, Temp, P, Hum;
+float waterThreshHold = 200;
+int waterMsgFlag = 0;
 
 // server
 //  Replace with your network credentials
@@ -63,6 +89,8 @@ String readBME280Pressure()
 //-----------------------need to modify
 void setup()
 {
+  //mail setup
+  
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, RXp2, TXp2);
 
@@ -119,6 +147,7 @@ void loop()
   // Serial.println(data);
   // Serial.println(".....");
   string_split(data);
+  sendMessgae();
   delay(3000);
 }
 
@@ -214,4 +243,89 @@ void string_split(String str)
       }
     }
   }
+}
+
+void smtpCallback(SMTP_Status status){
+  /* Print the current status */
+  Serial.println(status.info());
+
+  /* Print the sending result */
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      /* Get the result item */
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients);
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject);
+    }
+    Serial.println("----------------\n");
+  }
+}
+void sendMessgae(){
+  if( (waterMsgFlag == 0) && (WL.toFloat() > waterThreshHold)){
+    sendMsg("water level is high");
+    waterMsgFlag = 1;
+  }
+  if((waterMsgFlag == 1) && (WL.toFloat() < waterThreshHold)) {
+    waterMsgFlag = 0;
+  }
+}
+void sendMsg(String msg){
+  smtp.debug(1);
+
+  /* Set the callback function to get the sending results */
+  smtp.callback(smtpCallback);
+
+  /* Declare the session config data */
+  ESP_Mail_Session session;
+
+  /* Set the session config */
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  /* Declare the message class */
+  SMTP_Message message;
+
+  /* Set the message headers */
+  message.sender.name = "ESP";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "ESP Test Email";
+  message.addRecipient("Fahad", RECIPIENT_EMAIL);
+
+
+
+  
+  //Send raw text message
+  String textMsg = "Caution!\n"+msg;
+  message.text.content = textMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.text.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+  
+  message.priority = esp_mail_smtp_priority::esp_mail_smtp_priority_low;
+  message.response.notify = esp_mail_smtp_notify_success | esp_mail_smtp_notify_failure | esp_mail_smtp_notify_delay;
+
+  /* Set the custom message header */
+  //message.addHeader("Message-ID: <abcde.fghij@gmail.com>");
+
+  /* Connect to server with the session config */
+  if (!smtp.connect(&session))
+    return;
+
+  /* Start sending Email and close the session */
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.println("Error sending Email, " + smtp.errorReason());
 }
